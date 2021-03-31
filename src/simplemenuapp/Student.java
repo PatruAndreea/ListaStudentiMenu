2b568e928bde1968bdae8e40d4cd5d9089e52d34
PatruAndreea <81676560+PatruAndreea@users.noreply.github.com>
2021-03-31 19:04:40 +0300

    Add files via upload

---
+++ src/simplemenuapp/Student.java
@@ -0,0 +1,42 @@
+package simplemenuapp;
+
+public class Student
+{
+	private String id;
+	private String name;
+	private String firstName;
+	private String average;
+	
+	public String getId()
+	{
+		return id;
+	}
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public String getAverage()
+	{
+		return average;
+	}
+	public void setAverage(String average)
+	{
+		this.average = average;
+	}
+}
